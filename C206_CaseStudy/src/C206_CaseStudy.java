1b8df9229e6866ef052edf8234554ca16be8ce52
20014867 <20014867@myrp.edu.sg>
2021-08-03 17:43:23 +0800

    done by nicole

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,3 +1,10 @@
+/**
+ * We declare that this code was written by us.
+ * We will not copy or allow others to copy our code.
+ * We understand that copying code is considered as plagiarism.
+ *
+ * C206 Team 5, 2 Aug 2021 4:54:58 pm
+ */
 
 public class C206_CaseStudy {
 
