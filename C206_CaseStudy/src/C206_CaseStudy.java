3be23040f1a1a69f9da4e3f86249b74405b7af4e
20026389 <20026389@myrp.edu.sg>
2021-08-03 17:57:42 +0800

    ravi

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,8 +6,10 @@ public class C206_CaseStudy {
 		
 	}
 	public void RegistrationMenu() {
+		//ravi
 		System.out.println("1.Register for a tuition");
 		System.out.println("2.View all registrations");
 		System.out.println("3.Delete registration");
 	}
+	
 }
